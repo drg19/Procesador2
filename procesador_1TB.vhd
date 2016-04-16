185b8a8422c33763014f48d6a80affe59fbc5845
drg19 <drg_1995_@utp.edu.co>
2016-04-15 20:30:13 -0500

    Added files via upload

---
+++ procesador_1TB.vhd
@@ -0,0 +1,65 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use std.textio.all;
+
+
+ENTITY tb IS
+END tb;
+ 
+ARCHITECTURE behavior OF tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT pros1
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         ALUresult : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+
+ 	--Outputs
+   signal ALUresult : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 100 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: pros1 PORT MAP (
+          clk => clk,
+          reset => reset,
+          ALUresult => ALUresult
+        );
+		  
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period;
+		clk <= '1';
+		wait for clk_period;
+  end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      
+		
+		reset <= '0';
+      wait for 100 ns;
+		
+		reset <= '1';
+     
+
+      wait;
+   end process;
+
+END;
---
+++ pros2_Tb.vhd
@@ -0,0 +1,63 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ 
+ENTITY pros2_Tb IS
+END pros2_Tb;
+ 
+ARCHITECTURE behavior OF pros2_Tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT pros2
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         ALURESULT : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+
+ 	--Outputs
+   signal ALURESULT : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: pros2 PORT MAP (
+          clk => clk,
+          reset => reset,
+          ALURESULT => ALURESULT
+        );
+
+  -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period;
+		clk <= '1';
+		wait for clk_period;
+   end process;
+ 
+
+	
+   -- Stimulus process
+   stim_proc: process
+   begin		
+    	reset <= '0';
+		wait for 10 ns;
+		reset <= '1' ;
+		
+
+  
+      wait;
+   end process;
+
+END;
